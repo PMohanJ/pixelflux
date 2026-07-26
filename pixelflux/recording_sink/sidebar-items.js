e8194144edd1a5fe14cfe1a4279cb04d8bf507db
PMohanJ <PMohanJ@users.noreply.github.com>
2026-07-26 04:27:42 +0000

    deploy: a9f1993cec04d8873655a6c830e615d1ce0386e8

---
+++ pixelflux/recording_sink/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["RECORDING_SOCKET_ENV"],"struct":["RecordingSink"]};
---
+++ pixelflux/x11/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["run_capture"],"mod":["cursor"],"struct":["Controls"]};
